e45024e7b119ae121a8a39e99bc6301669387ee0
hiteshgit88 <hiteshpjadhav@gmail.com>
2019-12-31 15:27:52 +0530

    1 practice java commit

---
+++ src/main/java/ArmStrongNumber/ArmStrongNum.java
@@ -0,0 +1,26 @@
+package ArmStrongNumber;
+
+public class ArmStrongNum {
+
+    public static void main(String[] args) {
+        int n = 153, sum = 0, mod, num;
+        num=n;
+
+        while(n>0) //using while loop
+        {
+            mod = n%10;
+            n = n/10;
+            sum = sum + (mod*mod*mod);
+        }
+
+        if(num == sum){
+            System.out.println("Armstrong Number Confirmed");
+        }
+        else
+        {
+            System.out.println("Not Armstrong Number Confirmed");
+        }
+
+    }
+
+}
